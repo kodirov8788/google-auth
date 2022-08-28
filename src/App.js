b9508d04e4f5e1845aa1df1228903bdf9ec8b514
Muhammadali <muhammad@Muhammads-MacBook-Pro.local>
2022-08-28 15:49:27 +0500

    adasdas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "./firebase/firebaseConfig"
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 
 function App() {
+  const Auth = getAuth(firebase)
+  const provider = new GoogleAuthProvider()
+
+  const SignOut = () => {
+    localStorage.clear()
+    window.location.reload()
+  }
+  const SignInWithGoogle = () => {
+    signInWithPopup(Auth, provider)
+      .then(res => {
+        localStorage.setItem("Name", res.user.displayName)
+        localStorage.setItem("Email", res.user.email)
+        localStorage.setItem("Photo", res.user.photoURL)
+        console.log(res.user);
+        window.location.reload()
+      })
+      .catch(err => console.log(err))
+  }
+
+  const localName = localStorage.getItem("Name"),
+    localEmail = localStorage.getItem("Email"),
+    localPhoto = localStorage.getItem("Photo")
+  console.log(localName, localEmail, localPhoto);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ssalsdas</h1>
+
+      {localName === null ? <button onClick={SignInWithGoogle}>Sign in </button> : <button onClick={SignOut}>Sign Out </button>}
+
+      <h1>{localName}</h1>
+      <h1>{localEmail}</h1>
+      <img src={localPhoto} width="400px" alt="" />
     </div>
   );
 }
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBcpTbKOUpmG9uNEdrKMC0ZkPIyTrFI4oU",
+    authDomain: "blogs-f24cd.firebaseapp.com",
+    projectId: "blogs-f24cd",
+    storageBucket: "blogs-f24cd.appspot.com",
+    messagingSenderId: "628334680016",
+    appId: "1:628334680016:web:9a7520545cf0712c234bd5"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+export default app
+
+
